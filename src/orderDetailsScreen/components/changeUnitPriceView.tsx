e4730becb48ae767ffd78b2f05b94481f088a262
Eyad <eyiadhisham@yahoo.com>
2022-06-07 17:23:25 +0200

    ChangeUnitPriceView skeleton

---
+++ src/orderDetailsScreen/components/changeUnitPriceView.tsx
@@ -0,0 +1,12 @@
+import { FunctionComponent } from "react";
+import { Order } from "models/order";
+
+interface ChangeUnitPriceViewProps {
+    order:Order
+}
+ 
+const ChangeUnitPriceView: FunctionComponent<ChangeUnitPriceViewProps> = (props) => {
+    return ( <></> );
+}
+ 
+export default ChangeUnitPriceView;
